ace4ef354adc94599294a7d382ae29299e5b10c6
t4bbyp <tvbby.p@gmail.com>
2026-05-17 12:38:59 +0300

    updated the questions & answers

--- tori-quiz/src/assets/questions.js
+++ tori-quiz/src/assets/questions.js
@@ -1,20 +1,241 @@
 export const questions = [
   {
     id: "q1",
-    q: "Eres...?",
+    q: "Género",
     a: [
-      { id: "a", text: "Hombre" },
-      { id: "b", text: "Mujer" },
+      { id: "a", text: "Femenino" },
+      { id: "b", text: "Masculino" },
       { id: "c", text: "Otro" },
     ],
   },
   {
     id: "q2",
-    q: "Cual es tu color favorito?",
+    q: "Orientación sexual",
     a: [
-      { id: "a", text: "Azul" },
-      { id: "b", text: "Rosa" },
-      { id: "c", text: "Morado" },
+      { id: "a", text: "Hetero" },
+      { id: "b", text: "Homo" },
+      { id: "c", text: "Bi" },
+      { id: "d", text: "Otro" },
     ],
   },
+  {
+    id: "q3",
+    q: "Eres...",
+    a: [
+      { id: "a", text: "Introvertido" },
+      { id: "b", text: "Extrovertido" },
+      { id: "c", text: "Ambivertido" },
+    ],
+  },
+  {
+    id: "q4",
+    q: "Cuando se trata de trabajo/tareas, tú...",
+    a: [
+      { id: "a", text: "Cuanto antes, mejor." },
+      { id: "b", text: "Meh, ya lo haré mañana... (asi todo el mes)" },
+      {
+        id: "c",
+        text: "Me centro en lo más importante primero; lo demás ya veré.",
+      },
+    ],
+  },
+  {
+    id: "q5",
+    q: "Prefieres...",
+    a: [
+      { id: "a", text: "Caos, aventura, eventos inesperados" },
+      {
+        id: "b",
+        text: "Rutina, saber qué es lo siguiente, tener control sobre tu vida",
+      },
+      {
+        id: "c",
+        text: "Un poco de ambas. La tranquilidad y la calma son lo mejor, pero a veces viene bien un poco de adrenalina",
+      },
+    ],
+  },
+  {
+    id: "q6",
+    q: "En cuanto al contacto físico...",
+    a: [
+      { id: "a", text: "AY, SÍ, POR FAVOR. UN BESO Y MIL ABRAZOS PARA TODOS" },
+      {
+        id: "b",
+        text: "Con amigos/familia, vale, de vez en cuando. Con desconocidos, solo si las formalidades lo requieren; preferiblemente no.",
+      },
+      { id: "c", text: "Tócame un pelo y te mato." },
+    ],
+  },
+  {
+    id: "q7",
+    q: "Como pareja, serías...",
+    a: [
+      { id: "a", text: "Romántico" },
+      { id: "b", text: "Pragmático" },
+      { id: "c", text: "¿Depende del momento?" },
+    ],
+  },
+  {
+    id: "q8",
+    q: "Eres más...",
+    a: [
+      { id: "a", text: "Emocional" },
+      { id: "b", text: "Racional" },
+    ],
+  },
+  {
+    id: "q9",
+    q: "En contextos sociales, eres...",
+    a: [
+      { id: "a", text: "Educado" },
+      { id: "b", text: "Maleducado" },
+      { id: "c", text: "Depende del momento" },
+    ],
+  },
+  {
+    id: "q10",
+    q: "Moralmente eres...",
+    a: [
+      { id: "a", text: "Flexible" },
+      { id: "b", text: "Estricto" },
+    ],
+  },
+  {
+    id: "q11",
+    q: "Cuando hay conflicto, tiendes a ser...",
+    a: [
+      { id: "a", text: "Evitativo" },
+      { id: "b", text: "Resolutivo" },
+    ],
+  },
+  {
+    id: "q12",
+    q: "Prefieres el ligoteo...",
+    a: [
+      { id: "a", text: "Explícito" },
+      { id: "b", text: "Sutil" },
+    ],
+  },
+  {
+    id: "q13",
+    q: "Procesando las emociones, tú...",
+    a: [
+      {
+        id: "a",
+        text: "Te tomas tu tiempo. A veces te cuesta, o prefieres analizar las cosas con cuidado.",
+      },
+      {
+        id: "b",
+        text: "Te las tomas tal cual vienen. No tienes problema con sentirlas, sean buenas o malas, y cuanto antes las aceptes, mejor.",
+      },
+    ],
+  },
+  {
+    id: "q14",
+    q: "Realmente eres...",
+    a: [
+      { id: "a", text: "Inteligente" },
+      { id: "b", text: "Tonto" },
+      { id: "c", text: "Tienes cabeza, pero falta interés/recursos" },
+    ],
+  },
+  {
+    id: "q15",
+    q: "Los demás dirían que eres...",
+    a: [
+      { id: "a", text: "Buena persona" },
+      { id: "b", text: "Mala persona" },
+      { id: "c", text: "Bastante ambiguo, difícil de etiquetar" },
+    ],
+  },
+  {
+    id: "q16",
+    q: "En cuanto a los problemas ajenos, tú...",
+    a: [
+      { id: "a", text: "Ofreces apoyo emocional y te centras, antes que nada, en hacer que el otro se sienta mejor" },
+      { id: "b", text: "Buscas soluciones directamente. Solo un par de palabras dulces no cambiarán la situación." },
+    ],
+  },
+  {
+    id: "q17",
+    q: "Tu libido es...",
+    a: [
+      { id: "a", text: "Muy intenso" },
+      { id: "b", text: "Moderado" },
+      { id: "c", text: "Bajo o inexistente" },
+    ],
+  },
+  {
+    id: "q18",
+    q: "Buscas......",
+    a: [
+      { id: "a", text: "Líos de una noche" },
+      { id: "b", text: "Relaciones estables y duraderas" },
+    ],
+  },
+  {
+    id: "q19",
+    q: "Te gustaría alguien mas...",
+    a: [
+      { id: "a", text: "Dominante" },
+      { id: "b", text: "Sumiso" },
+      { id: "c", text: "Adaptable" },
+    ],
+  },
+  {
+    id: "q20",
+    q: "¿Quieres hijos?",
+    a: [
+      { id: "a", text: "Sí" },
+      { id: "b", text: "No" },
+      { id: "c", text: "No estoy segur@..." },
+    ],
+  },
+  {
+    id: "q21",
+    q: "Prefieres las relaciones......",
+    a: [
+      { id: "a", text: "Monógamas" },
+      { id: "b", text: "Poliamorosas" },
+      { id: "c", text: "Abiertas" },
+      { id: "d", text: "Ninguna, gracias." },
+      { id: "e", text: "Da igual." },
+    ],
+  },
+  {
+    id: "q22",
+    q: "Tu estilo de apego es...",
+    a: [
+      { id: "a", text: "Seguro" },
+      { id: "b", text: "Evitativo" },
+      { id: "c", text: "Ansioso" },
+      { id: "d", text: "Desorganizado" },
+      { id: "e", text: "No me gusta la gente." },
+    ],
+  },
+  {
+    id: "q23",
+    q: "Y te gustaría que tu pareja fuera...",
+    a: [
+      { id: "a", text: "Seguro" },
+      { id: "b", text: "Evitativo" },
+      { id: "c", text: "Ansioso" },
+      { id: "d", text: "Desorganizado" },
+      { id: "e", text: "No le gusta la gente... y a ti te gusta sufrir, claramente." },
+    ],
+  },
+  {
+    id: "q24",
+    q: "Tu lenguaje amoroso:",
+    a: [
+      { id: "a", text: "Palabras de afirmación" },
+      { id: "b", text: "Tiempo de calidad" },
+      { id: "c", text: "Recibir/dar regalos" },
+      { id: "d", text: "Pingüinada (regalar objetos cotidianos o pequeños gestos)" },
+      { id: "e", text: "Actos de servicio" },
+      { id: "f", text: "Contacto físico" },
+      { id: "a", text: "Ninguno de estos" },
+    ],
+  },
+
 ];
