6d853f4cefddfca0b4a05be55fd78081d18b6f16
t4bbyp <tvbby.p@gmail.com>
2026-05-31 17:39:27 +0300

    fix faq bug v2

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -10,12 +10,37 @@ export default function FaqPage() {
       <h2>FaQ</h2>
 
       <Accordion title={t(($) => $.faq.q1)} content={t(($) => $.faq.a1)} />
-      <Accordion title={t(($) => $.faq.q2)} content={<FaqString stringKey="faq.a2" link="https://scriptamanent.foroactivo.com/" />} />
-      <Accordion title={t(($) => $.faq.q3)} content={<FaqString stringKey="faq.a3" />} />
-      <Accordion title={t(($) => $.faq.q4)} content={<FaqString stringKey="faq.a4" />} />
+      <Accordion
+        title={t(($) => $.faq.q2)}
+        content={
+          <FaqString
+            stringKey="faq.a2"
+            link="https://scriptamanent.foroactivo.com/"
+          />
+        }
+      />
+      <Accordion
+        title={t(($) => $.faq.q3)}
+        content={<FaqString stringKey="faq.a3" />}
+      />
+      <Accordion
+        title={t(($) => $.faq.q4)}
+        content={<FaqString stringKey="faq.a4" />}
+      />
       <Accordion title={t(($) => $.faq.q5)} content={t(($) => $.faq.a5)} />
-      <Accordion title={t(($) => $.faq.q6)} content={<FaqString stringKey="faq.a6" link="https://picrew.me/en/image_maker/1108773" />} />
-      <Accordion title={t(($) => $.faq.q7)} content={<FaqString stringKey="faq.a7" link="mailto:asaaphsm@gmail.com" />} />
+      <Accordion
+        title={t(($) => $.faq.q6)}
+        content={
+          <FaqString
+            stringKey="faq.a6"
+            link="https://picrew.me/en/image_maker/1108773"
+          />
+        }
+      />
+      <Accordion
+        title={t(($) => $.faq.q7)}
+        content={<FaqString stringKey="faq.a7" link="mailto:asaaphsm@gmail.com" />}
+      />
     </>
   );
 }
